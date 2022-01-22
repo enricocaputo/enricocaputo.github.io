b70d9a348a9840acc353aa89491e47de01b26152
Enrico Caputo <enricocaputo.1972@gmail.com>
2022-01-22 17:35:25 +0100

    workbox

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,21 @@
 module.exports = {
-  "globDirectory": "/",
-  "globPatterns": [
-    "**/*.{ico,png,jpg,svg,webp,js}"
-  ],
-  "swDest": "/sw.js"
+	globDirectory: '/',
+	globPatterns: [
+		'**/*.{ico,png,jpg,svg,webp,js}'
+	],
+	swDest: '/sw.js',
+	runtimeCaching: [{
+		urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+		handler: 'CacheFirst',
+		options: {
+			cacheName: 'images',
+				expiration: {
+				maxEntries: 22,
+			},
+		},
+	}],
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
 };
