c98589e06b251c12336ea46572579f0ddfb473e5
Enrico Caputo <enricocaputo.1972@gmail.com>
2022-02-02 17:50:40 +0100

    c

--- js/carousel.min.js
+++
@@ -1,1 +0,0 @@
-var myIndex=0;function carousel(){var e,l=document.getElementsByClassName("mySlides");for(e=0;e<l.length;e++)l[e].style.display="none";++myIndex>l.length&&(myIndex=1),l[myIndex-1].style.display="block",setTimeout(carousel,2e3)}carousel();
